175c54e98ca744c28e7eb4c0160362138ba97b4f
baiqiqi <baiqiqi@ascs.tech>
2018-05-08 09:43:33 +0800

    Initial commit

---
+++ src/main/java/com/baiqiqi/thread_and_concurrency/ThreadAndConcurrencyApplication.java
@@ -0,0 +1,12 @@
+package com.baiqiqi.thread_and_concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadAndConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadAndConcurrencyApplication.class, args);
+    }
+}
---
+++ src/test/java/com/baiqiqi/thread_and_concurrency/ThreadAndConcurrencyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.baiqiqi.thread_and_concurrency;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThreadAndConcurrencyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
