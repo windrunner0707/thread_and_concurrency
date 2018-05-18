ce8014fa5572675382341590baa37e7a186dd3dd
baiqiqi <baiqiqi@ascs.tech>
2018-05-18 15:45:53 +0800

    Add lock.

--- src/main/java/com/baiqiqi/thread_and_concurrency/example/lock/LockExample5.java
+++ src/main/java/com/baiqiqi/thread_and_concurrency/example/lock/LockExample5.java
@@ -1,4 +1,59 @@
 package com.baiqiqi.thread_and_concurrency.example.lock;
 
+import com.baiqiqi.thread_and_concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.StampedLock;
+
+/**
+ * StampedLock该类是一个读写锁的改进，它的思想是读写锁中读不仅不阻塞读，同时也不应该阻塞写
+ * 使用了自旋锁
+ */
+@Slf4j
+@ThreadSafe
 public class LockExample5 {
+
+    // 请求总数
+    public static int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public static int count = 0;
+
+    private final static StampedLock lock = new StampedLock();
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal ; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}", count);
+    }
+
+    private static void add() {
+        long stamp = lock.writeLock();
+        try {
+            count++;
+        } finally {
+            lock.unlock(stamp);
+        }
+    }
 }
